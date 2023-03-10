0a7d1c750477f37a4ce01c705471358237fa1531
Sebastián Pizarro <sebastian_p_b@hotmail.com>
2023-03-10 06:02:12 -0300

    Clase 20: incorporación de menú de hamburguesa

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
 const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+
+const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toogleDesktopMenu);
 menuHamIcon.addEventListener('click', toogleMobileMenu);
+menuCarritoIcon.addEventListener('click', toogleCarritoAside);
 
 function toogleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+    mobileMenu.classList.add('inactive');
+    aside.classList.add('inactive');
 }
 
 function toogleMobileMenu(){
     mobileMenu.classList.toggle('inactive');
+    desktopMenu.classList.add('inactive');
+    aside.classList.add('inactive');
+}
+
+function toogleCarritoAside(){
+    aside.classList.toggle('inactive');
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
 }
